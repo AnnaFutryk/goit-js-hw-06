55559e5e62458217e21ede8e6fe7783896da878c
AnnaFutryk <anna.futryk@gmail.com>
2023-04-27 17:16:11 +0300

    Create task-06.js

---
+++ src/js/task-06.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', onInputBlur);
+
+function onInputBlur() {
+    const validLength = input.dataset.length;
+    const inputValueLength = input.value.length;
+    
+    if (inputValueLength === Number(validLength)) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+    } else {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    };
+}
