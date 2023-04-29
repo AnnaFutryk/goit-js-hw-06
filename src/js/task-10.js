fb129d23e256d35b2012015ee072a1e5dc7d5865
AnnaFutryk <anna.futryk@gmail.com>
2023-04-29 13:21:17 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const boxesContainer = document.getElementById('boxes');
+const createBtn = document.querySelector('[data-create]');
+const removeBtn = document.querySelector('[data-destroy]');
+
+createBtn.addEventListener('click', createBoxes);
+removeBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const createdEl1 = document.createElement('div');
+    createdEl1.style.width = size + 'px';
+    createdEl1.style.height = size + 'px';
+    createdEl1.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.append(createdEl1);
+    size += 10;
+  };
+  
+};
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+};
