5cb56f01086225f4f640003943d82da2eca9f752
AnnaFutryk <anna.futryk@gmail.com>
2023-04-29 13:21:12 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', onClickChangeColor);
+
+function onClickChangeColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+}
