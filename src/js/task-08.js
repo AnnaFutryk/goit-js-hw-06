5046299da917f6a97f8daf33de479404ac2c775f
AnnaFutryk <anna.futryk@gmail.com>
2023-04-29 13:21:08 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!');
+    }
+
+    const formData = new FormData(event.currentTarget);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+
+    console.log(formDataObject);
+
+  event.currentTarget.reset();
+}
+
 
