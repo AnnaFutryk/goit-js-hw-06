dea2ffa9dae51cbfd1b6acb3537ca4eaa6a060a9
AnnaFutryk <anna.futryk@gmail.com>
2023-04-30 13:06:31 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,11 @@
 const input = document.getElementById('font-size-control');
 const textEl = document.getElementById('text');
 
+textEl.style.fontSize = input.value + 'px';
+
 input.addEventListener('input', onInputChange);
 
 function onInputChange() {
-    textEl.style.fontSize = input.value + 'px';
+    textEl.style.fontSize = input.value + 'px';;
 }
+
